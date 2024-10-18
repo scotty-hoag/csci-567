cd3286485d4697d0419100fafd8b14d2add91e5a
Scotty Hoag <scotty.hoag@gmail.com>
2024-10-17 20:55:29 -0700

    Adding FEATURE-5 in champion_data.csv.

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # csci-567
 Repository for the USC CSCI-567 semester group project.
 
-Features defined in the paper are calculated and found in the following places:
+Features defined in the paper are calculated and placed in the following places:
 
 1. playerRole_i,j: player_data.csv, "Win Ratio"
+
+2. 
+
+3.
+
+4.
+
+5. championRole_i,j: champion_data.csv, "Win Ratio"
+
+6. 
+
+7. 
+
+8. 
--- makedata.py
+++ makedata.py
@@ -51,8 +51,15 @@ glob_players = {}
 class Champion:
     name = ""
 
+    gamesPlayed = 0
+    gamesWon = 0
+
     rolesPlayed = {}
     rolesWon = {}
+
+    def __init__(self):
+        self.rolesPlayed = {}
+        self.rolesWon = {}
 glob_champions = {}
 
 def IsRowValid(row):
@@ -61,23 +68,13 @@ def IsRowValid(row):
     return bValid
 
 with open('lol_data/matchinfo.csv', newline='', encoding='utf-8') as csvFile:
-    #csvReader = csv.reader(csvFile, delimiter=',', quotechar='|')
-    #for row in reader:
-    #    print(', '.join(row))
-    
-    #for role in glob_rolesRed:
-    #    numMatchesInRole[role] = {}
-    #    numMatchesInRoleWon[role] = {}
-    #for role in rolesBlue:
-    #    numMatchesInRole[role] = {}
-    #    numMatchesInRoleWon[role] = {}
-
     reader = csv.DictReader(csvFile)
     for row in reader:
         # For now, skip rows that don't have complete information.
         if not IsRowValid(row):
             continue
 
+        # Player and champion names are added to SETs, so enforces uniqueness.
         for role in glob_rolesAll:
             glob_playerNames.add(row[role])
 
@@ -88,6 +85,7 @@ with open('lol_data/matchinfo.csv', newline='', encoding='utf-8') as csvFile:
         glob_numMatchesInRole[role] = {}
         glob_numMatchesInRoleWon[role] = {}
 
+    # Record the player role and champion win stats.
     for playerName in glob_playerNames:
         player = Player()
         player.name = playerName
@@ -99,9 +97,20 @@ with open('lol_data/matchinfo.csv', newline='', encoding='utf-8') as csvFile:
             player.rolesWon[role] = 0
 
         # Record the number of champion plays and wins.
-        for champ in glob_champsAll:
+        for champ in glob_championNames:
             player.championsPlayed[champ] = 0
             player.championsWon[champ] = 0
+    
+    # Record the champion role win stats.
+    for championName in glob_championNames:
+        champion = Champion()
+        champion.name = championName
+        glob_champions[championName] = champion
+
+        # Init the number of plays/wins 
+        for role in glob_rolesAll:
+            champion.rolesPlayed[role] = 0
+            champion.rolesWon[role] = 0
 
 with open('lol_data/matchinfo.csv', newline='', encoding='utf-8') as csvFile:
     reader = csv.DictReader(csvFile)
@@ -133,39 +142,51 @@ with open('lol_data/matchinfo.csv', newline='', encoding='utf-8') as csvFile:
         
         for roleIndex in range(len(losingTeamRoles)):
             role = losingTeamRoles[roleIndex]
-            champion = losingTeamChamps[roleIndex]
             playerName = row[role]
             player = glob_players[playerName]
 
-            # Inc matches played by the player.
-            player.gamesPlayed += 1
+            championRole = losingTeamChamps[roleIndex]
+            championName = row[championRole]
+            champion = glob_champions[championName]
 
-            # Inc matches played by the player in the specific role.
+            # Inc matches played by the player/champion.
+            player.gamesPlayed += 1
+            champion.gamesPlayed += 1
+
+            # Inc matches played by the player/champion in the specific role.
             player.rolesPlayed[role] += 1
+            champion.rolesPlayed[role] += 1
 
             # Inc matches played by the player with the specific Champion.
-            player.championsPlayed[champion] += 1
+            player.championsPlayed[championName] += 1
 
         for roleIndex in range(len(winningTeamRoles)):
             role = winningTeamRoles[roleIndex]
-            champion = winningTeamChamps[roleIndex]
             playerName = row[role]
             player = glob_players[playerName]
+
+            championRole = winningTeamChamps[roleIndex]
+            championName = row[championRole]
+            champion = glob_champions[championName]
             
-            # Inc matches played and won by the player.
+            # Inc matches played and won by the player/champion.
             player.gamesPlayed += 1
             player.gamesWon += 1
+            champion.gamesPlayed += 1
+            champion.gamesWon += 1
 
-            # Inc matches played and won by the player in the specific role.
+            # Inc matches played and won by the player/champion in the specific role.
             player.rolesPlayed[role] += 1
             player.rolesWon[role] += 1
+            champion.rolesPlayed[role] += 1
+            champion.rolesWon[role] += 1
 
             # Inc matches played and won by the player with the specific Champion.
-            player.championsPlayed[champion] += 1
-            player.championsWon[champion] += 1
+            player.championsPlayed[championName] += 1
+            player.championsWon[championName] += 1
 
 def WritePlayerData():
-    outputString = "Player Name," + \
+    outputString = "Name," + \
         "Games Played,Games Won,Win Ratio," + \
         "Blue Top Plays,Red Top Plays,Top Plays," + \
         "Blue Top Wins,Red Top Wins,Top Wins," + \
@@ -204,7 +225,43 @@ def WritePlayerData():
     file.close()
 
 def WriteChampionData():
-    pass
+    outputString = "Name," + \
+        "Games Played,Games Won,Win Ratio," + \
+        "Blue Top Plays,Red Top Plays,Top Plays," + \
+        "Blue Top Wins,Red Top Wins,Top Wins," + \
+        "Blue Jungle, Red Jungle, Jungle," + \
+        "Blue Jungle Wins, Red Jungle Wins, Jungle Wins," + \
+        "Blue Middle, Red Middle, Middle," + \
+        "Blue Middle Wins, Red Middle Wins, Middle Wins," + \
+        "Blue ADC, Red ADC, ADC," + \
+        "Blue ADC Wins, Red ADC Wins, ADC Wins," + \
+        "Blue Support, Red Support, Support," + \
+        "Blue Support Wins, Red Support Wins, Support Wins," + \
+        "\n"
+
+    rBlue = glob_rolesBlue
+    rReds = glob_rolesRed 
+    for championName, champion in glob_champions.items():
+        if champion == None or champion.name == "":
+            continue
+        championWinRatio = ((champion.gamesWon/champion.gamesPlayed) if (champion.gamesPlayed > 0) else 0)
+        outputString += "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format( \
+            champion.name \
+            , champion.gamesPlayed, champion.gamesWon, championWinRatio \
+            , champion.rolesPlayed[rBlue[0]], champion.rolesPlayed[rReds[0]], champion.rolesPlayed[rBlue[0]] + champion.rolesPlayed[rReds[0]] \
+            , champion.rolesWon[   rBlue[0]], champion.rolesWon[   rReds[0]], champion.rolesWon   [rBlue[0]] + champion.rolesWon[   rReds[0]] \
+            , champion.rolesPlayed[rBlue[1]], champion.rolesPlayed[rReds[1]], champion.rolesPlayed[rBlue[1]] + champion.rolesPlayed[rReds[1]] \
+            , champion.rolesWon[   rBlue[1]], champion.rolesWon[   rReds[1]], champion.rolesWon   [rBlue[1]] + champion.rolesWon[   rReds[1]] \
+            , champion.rolesPlayed[rBlue[2]], champion.rolesPlayed[rReds[2]], champion.rolesPlayed[rBlue[2]] + champion.rolesPlayed[rReds[2]] \
+            , champion.rolesWon[   rBlue[2]], champion.rolesWon[   rReds[2]], champion.rolesWon   [rBlue[2]] + champion.rolesWon[   rReds[2]] \
+            , champion.rolesPlayed[rBlue[3]], champion.rolesPlayed[rReds[3]], champion.rolesPlayed[rBlue[3]] + champion.rolesPlayed[rReds[3]] \
+            , champion.rolesWon[   rBlue[3]], champion.rolesWon[   rReds[3]], champion.rolesWon   [rBlue[3]] + champion.rolesWon[   rReds[3]] \
+            , champion.rolesPlayed[rBlue[4]], champion.rolesPlayed[rReds[4]], champion.rolesPlayed[rBlue[4]] + champion.rolesPlayed[rReds[4]] \
+            , champion.rolesWon[   rBlue[4]], champion.rolesWon[   rReds[4]], champion.rolesWon   [rBlue[4]] + champion.rolesWon[   rReds[4]] \
+            )
+    file = open("feature_data/champion_data.csv", 'w+')
+    file.write(outputString)
+    file.close()
 
 WritePlayerData()
 WriteChampionData()
