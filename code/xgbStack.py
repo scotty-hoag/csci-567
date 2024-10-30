2d502e46ad8a5c8e7337a25fb174f922e582f28f
rpabo <rjpabo@gmail.com>
2024-10-29 19:12:54 -0700

    Additional implementation for xgbStack.py

--- code/xgbStack.py
+++ code/xgbStack.py
@@ -2,18 +2,31 @@ import time
 
 import numpy as np
 import pandas as pd
+
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import StackingClassifier #For XGB implementation
 
 from concurrent.futures import ThreadPoolExecutor
 
 import load_data
 
 class xgbStack:
-    def __init__(self, hpNaiveBayes=None, hpNeuralNetork=None, hpXgb=None):
+
+    def __init__(self, hpNaiveBayes=None, hpNeuralNetwork=None, hpXgb=None):
+        """
+            Input:
+                hpNaiveBayes - dict: Dictionary whose keys represent the hyperparameters to 
+                    set for naive bayes.
+                hpNeuralNetwork - dict: Dictionary whose keys represent the hyperparameters
+                    to set to for the neural network. 
+                hpXbg - dict: Dictionary whose keys represent the hyperparameters to set
+                    for extreme gradient boost.
+
+        """
         pass
 
     def train_and_predict(self, model, X_train, y_train, X_test):
@@ -22,14 +35,27 @@ class xgbStack:
     
     #Import the data, either by processing all the feature csv files along with the matchdata.csv file or 
     #inputting a precompiled input file.
-    def import_data(self):
-        pass
+    def import_data(self, bPerformZNormalization=True, bGenerateOutputFile=False, 
+                    bIncludeChampionRole_Feature=False):
+        df_input = load_data.load_data(bPerformZNormalization=bPerformZNormalization, 
+                                       bGenerateOutputFile=bGenerateOutputFile, 
+                                       bIncludeChampionRole_Feature=bIncludeChampionRole_Feature)
+        
+        return df_input
     
     #Split the data into training and test sets.
     def split_data(self, dataFrame):
+        """
+            Input: Dataframe containing all the labels referenced in the research (optionally
+             including feature 5), along with the bResult label.
+        """
         pass
 
     def parallelExecute_LearningModels(self):
+        """
+            Execute both the naive bayes model and NN concurrently.
+
+        """
         with ThreadPoolExecutor() as executor:
             pass
 
@@ -40,17 +66,27 @@ class xgbStack:
             # predictions_1 = future_1.result()
             # predictions_2 = future_2.result()
         
-    def execute_metaModel(self):
+
+        #Return a tuple containing the generated dataframe predictions for each base model.
+
+        
+    def execute_metaModel(self, tuple_df_baseModelPredictions):
+        #Refer to implementation described at: https://scikit-learn.org/dev/modules/generated/sklearn.ensemble.StackingClassifier.html 
+        
+        #xgb_model = StackingClassifier()
+
         pass
         
     def train_model(self):
 
-        self.import_data() #Should return a dataframe containing the labels mentioned in the research paper
+        self.import_data() 
         self.split_data()
-        #self.parallelExecute_LearningModels()
+        self.parallelExecute_LearningModels()
         self.execute_metaModel()
-        
-        
+
+        #Return dataframe of final predictions.
+
 
 if __name__ == "__main__":
-    pass    
+    modelInstance = xgbStack()
+    entry = modelInstance.import_data()
